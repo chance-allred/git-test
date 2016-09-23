bfb8fcf9834edfd1511b9faa96c1553c35351be3
Chance Allred <chance@redcodesolutions.com>
2016-09-23 00:57:36 -0700

    Added some nonsense

--- git-test/ViewController.swift
+++ git-test/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let num = 1
+        print(num)
     }
 
     override func didReceiveMemoryWarning() {
