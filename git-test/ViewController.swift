078f33a537d20e62bb08e6d61dc7b8ed32f38839
Chance Allred <chance@redcodesolutions.com>
2016-09-23 00:08:48 -0700

    First Commit

---
+++ git-test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  git-test
+//
+//  Created by Chance Allred on 9/23/16.
+//  Copyright © 2016 Chance Allred. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
